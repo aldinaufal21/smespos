c0de28ccc348db835c262f66b588d8c2f33adf28
Ilham Fadhilah <ilhamfi_2701@yahoo.com>
2020-09-26 09:50:36 +0700

    form template for user

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -8,5 +8,28 @@
     <h1 class="h3 mb-0 text-gray-800">Add New User</h1>
   </div>
 
+  <div class="card shadow mb-4">
+    <div class="card-body">
+      <form>
+        <div class="form-group row">
+          <label for="js-input-username" class="col-sm-2 col-form-label">Username</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="js-input-username" placeholder="Username">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="js-input-password" class="col-sm-2 col-form-label">Password</label>
+          <div class="col-sm-10">
+            <input type="password" class="form-control" id="js-input-password" placeholder="Password">
+          </div>
+        </div>
+        <div class="form-group row">
+          <div class="col-sm-10 offset-2">
+            <button type="submit" class="btn btn-primary float-right">Add</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
 </div>
 @endsection
