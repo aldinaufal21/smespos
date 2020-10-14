44ed93231f6f9415e002e82fd013db1a94f76c94
Patrick Telnoni <ptelnoni89@gmail.com>
2020-10-15 00:02:20 +0700

    removing unnecessary

--- app/Kasir.php
+++ app/Kasir.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Kasir extends Model
 {
-    //
     public function transaksiKasir()
     {
         return $this->hasMany('App\Models\TransaksiKasir');
